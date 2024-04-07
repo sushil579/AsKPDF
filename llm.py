5834b6f79d4ec514299675cab88bfad7f73f5fb8
dkr <deepukreddy123@gmail.com>
2024-04-07 22:04:31 +0200

    message_init

--- llm.py
+++ llm.py
@@ -7,29 +7,31 @@ from utils import make_init_message
 
 message = []
 
+
 class openAIGPT():
 
-    def __init__(self ,  model="gpt-3.5-turbo"):
+    def __init__(self,  model="gpt-3.5-turbo"):
         load_dotenv(".env")
-        api_key = os.environ["OPENAI_API_KEY"] 
+        api_key = os.environ["OPENAI_API_KEY"]
         self.client = OpenAI(api_key=api_key)
         self.model = model
 
-    def embed_documents(self , documents):
+    def embed_documents(self, documents):
         embeddings = OpenAIEmbeddings()
         self.vector_db = FAISS.from_documents(documents, embeddings)
 
-    def similarity_search(self , query):
+    def similarity_search(self, query):
         self.query = query
-        retriever = self.vector_db.as_retriever(search_type="similarity_score_threshold" , search_kwargs={"score_threshold": 0.5 , "k":2})
+        retriever = self.vector_db.as_retriever(
+            search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.5, "k": 2})
         res_docs = retriever.get_relevant_documents(query)
         return res_docs
-   
-    def completion(self, res_docs ):
-        messages = make_init_message(res_docs , self.query)
+
+    def completion(self, messages):
+
         completion = self.client.chat.completions.create(
-        model=self.model,
-        messages=messages
+            model=self.model,
+            messages=messages
         )
 
         return completion.choices[0].message
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 
 from llm import openAIGPT
-from utils import pdf_loader
+from utils import make_init_message, pdf_loader
 
 
-pdf_path = "GeneralBiology.pdf"
+pdf_path = r"C:\Users\Daku\Desktop\chatPDF\ChatPDF\GeneralBiology.pdf"
 pages = pdf_loader(pdf_path)
 
 gpt = openAIGPT()
 gpt.embed_documents(pages)
 
-res_docs = gpt.similarity_search("Nature of science")
+query = "Nature of science"
+res_docs = gpt.similarity_search(query)
+messages = make_init_message(res_docs, query)
+
+
 output = gpt.completion(res_docs)
